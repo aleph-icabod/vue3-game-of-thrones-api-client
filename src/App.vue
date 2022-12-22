a837d04efaee4e44379cc4a8579d823618282990
Miguel Bautista <angel_507@live.com.mx>
2022-12-22 12:25:17 -0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="wrapper">
+    <h1>Hello</h1>
+    <p>world</p>
+  </div>
+</template>
+
+<style scoped>
+
+.wrapper{
+  display: flex;
+  flex-direction: column;
+}
+
+</style>
